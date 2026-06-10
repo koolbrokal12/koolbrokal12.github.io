4dcfcd04b51ed586410345b514ad0b0fbd9c04c0
koolbrokal12 <kalvin.ponce@jpschools.org>
2026-06-10 15:13:16 +0000

    nothing i do will be perfect nor succesful in life

--- fsd-projects/runtime/scenery.js
+++ fsd-projects/runtime/scenery.js
@@ -5,20 +5,3 @@
 */
 
 // TODO 1: Create more scenery instances
-const scenery = {
-  moon: {
-    imageUrl: "images/backgrounds/moon.png",
-    loopWidth: 0,
-    instances: [{ x: 100, y: 175, width: 150, height: 150 }],
-  },
-  building: {
-    imageUrl: "images/backgrounds/building.png",
-    loopWidth: 1400,
-    instances: [{ x: 400, width: 100, height: 300, speedX: -2 }],
-  },
-  lamp: {
-    imageUrl: "images/backgrounds/lamp.png",
-    loopWidth: 1400,
-    instances: [{ x: 700, width: 50, height: 150, speedX: 0 }],
-  },
-};
