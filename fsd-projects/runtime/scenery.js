6098a1e14a7febcf4dac593cff42b3b113945001
koolbrokal12 <kalvin.ponce@jpschools.org>
2026-06-12 14:49:30 +0000

    internal message

--- fsd-projects/runtime/scenery.js
+++ fsd-projects/runtime/scenery.js
@@ -4,4 +4,25 @@
     The background images will be drawn in order from top to bottom, so put the ones in the far background first, then work forward. Note that none of the background images can go in front of Hallebot.
 */
 
-// TODO 1: Create more scenery instances
+// scenery configurations for parallax scrolling
+// adding different widths and speeds to make it look dimensional
+var scenery = {
+    building: {
+        loopWidth: 1200,
+        instances: [
+            { x: 0, width: 100, height: 300, speedX: -2 },
+            { x: 300, width: 150, height: 400, speedX: -1.5 },
+            { x: 650, width: 120, height: 250, speedX: -2.2 },
+            { x: 900, width: 180, height: 450, speedX: -1.2 }
+        ]
+    },
+    lamp: {
+        loopWidth: 800,
+        instances: [
+            { x: 150, width: 20, height: 100, speedX: -3 },
+            { x: 450, width: 20, height: 100, speedX: -3 },
+            { x: 750, width: 20, height: 100, speedX: -3 },
+            { x: 1050, width: 20, height: 100, speedX: -3 }
+        ]
+    }
+};
--- fsd-projects/runtime/student.js
+++ fsd-projects/runtime/student.js
@@ -1,31 +1,125 @@
+// TODO 2: moveScenery
 function moveScenery() {
-  // TODO 2: Move background scenery based on current level speed
+    // loop through all building objects
+    for (var i = 0; i < scenery.building.instances.length; i++) {
+        var buildingInstance = scenery.building.instances[i];
+        
+        // update position based on speeds
+        buildingInstance.x = buildingInstance.x + buildingInstance.speedX + currentLevel.speed;
+        
+        // if it goes off screen left, reset it to the right side loop point
+        if (buildingInstance.x + buildingInstance.width < 0) {
+            buildingInstance.x = scenery.building.loopWidth;
+        }
+    }
+
+    // loop through all lamp objects
+    for (var j = 0; j < scenery.lamp.instances.length; j++) {
+        var lampInstance = scenery.lamp.instances[j];
+        
+        lampInstance.x = lampInstance.x + lampInstance.speedX + currentLevel.speed;
+        
+        if (lampInstance.x + lampInstance.width < 0) {
+            lampInstance.x = scenery.lamp.loopWidth;
+        }
+    }
 }
 
+// TODO 3: generateLevel
 function generateLevel() {
-  // TODO 3: Generate the current level's game objects
+    for (var i = 0; i < currentLevel.gameObjects.length; i++) {
+        var currentObject = currentLevel.gameObjects[i];
+        
+        // call the factory function to build it
+        create(currentObject);
+        
+        // console.log("type: " + currentObject.type + ", kind: " + currentObject.kind); // commented out for final check
+    }
 }
 
+// TODO 4: Create
 function create(obj) {
-  // TODO 4: Create a game object based on its type and kind
+    // check type and trigger the matching maker function
+    if (obj.type === "obstacle") {
+        makeObstacle(obj);
+    } 
+    else if (obj.type === "enemy") {
+        makeEnemy(obj);
+    } 
+    else if (obj.type === "powerup") {
+        makePowerup(obj);
+    } 
+    else if (obj.type === "goal") {
+        makeGoal(obj);
+    } 
+    else if (obj.type === "platform") {
+        makePlatform(obj);
+    }
 }
 
+// TODO 5: Filter
 function filterObjects(type) {
-  // TODO 5: Return only the game objects of the specified type
+    var matched = [];
+    
+    for (var i = 0; i < gameObjects.length; i++) {
+        if (gameObjects[i].type === type) {
+            matched.push(gameObjects[i]);
+        }
+    }
+    
+    return matched;
 }
 
-function moveGameObjects(objectList) {
-  // TODO 6: Move all game objects of a single type based on speeds
+// TODO 6: moveGameObjects
+function moveGameObjects(objectsArray) {
+    for (var i = 0; i < objectsArray.length; i++) {
+        var currentObject = objectsArray[i];
+        
+        // moves objects left as hallebot moves right
+        currentObject.x = currentObject.x + currentObject.speedX - currentLevel.speed;
+        currentObject.y = currentObject.y + currentObject.speedY;
+    }
 }
 
+// TODO 8: handleProjectileCollisions
 function handleProjectileCollisions() {
-  // TODO 8: Handle collisions between projectiles and enemies
+    for (var i = 0; i < gameObjects.length; i++) {
+        var currentObject = gameObjects[i];
+        
+        // nested loop for the lasers/projectiles
+        for (var j = 0; j < projectiles.length; j++) {
+            var currentProjectile = projectiles[j];
+            
+            if (isCollidingWithProjectile(currentProjectile, currentObject) === true) {
+                handleProjectileObjectCollision(i, j);
+            }
+        }
+    }
 }
 
+// TODO 9: handleHallebotGenericCollisions
 function handleHallebotGenericCollisions() {
-  // TODO 9: Handle collisions between Hallebot and game objects
+    for (var i = 0; i < gameObjects.length; i++) {
+        var currentObject = gameObjects[i];
+        
+        // don't check collision if it's a platform layout element
+        if (currentObject.type !== "platform") {
+            if (isGenericCollision(currentObject) === true) {
+                handleHallebotGenericCollision(i);
+            }
+        }
+    }
 }
 
+// TODO 10: triggerLevelTransition
 function triggerLevelTransition() {
-  // TODO 10: Transition to the next level or show win screen
+    currentLevelIndex = currentLevelIndex + 1;
+    gameObjects = []; // wipe previous level elements
+    
+    if (currentLevelIndex >= levels.length) {
+        player.winConditionMet = true;
+    } else {
+        currentLevel = levels[currentLevelIndex];
+        generateLevel();
+    }
 }
